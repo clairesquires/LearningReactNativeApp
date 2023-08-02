ffadefb1775be83e6c2bdb1e60ac5f7154f188f6
Claire Squires <claires@theodo.co.uk>
2023-08-02 19:44:59 +0100

    feat: add pressable to alert

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
 import { HomeScreen } from '../src/screens/HomeScreen';
+import { Text } from 'react-native';
 
 export default function App() {
   return <HomeScreen />;
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,20 +1,21 @@
-import { View, Text } from 'react-native';
+import { View, Text, Pressable } from 'react-native';
 import { styles } from '../styles';
 import { Link } from 'expo-router';
 
-export const HomeScreen = ({ navigation }: any) => (
-  <>
-    <View style={[styles.container, styles.home]}>
-      <Text style={styles.text}>This is my home screen :)</Text>
-      <Link href="/profile" style={styles.button}>
-        <Text style={styles.buttonText}>See profile</Text>
-      </Link>
-      <Link href="/overflowing" style={styles.button}>
-        <Text style={styles.buttonText}>See overflowing</Text>
-      </Link>
-      <Link href="/safeView" style={styles.button}>
-        <Text style={styles.buttonText}>See not overflowing</Text>
-      </Link>
-    </View>
-  </>
+export const HomeScreen = () => (
+  <View style={[styles.container, styles.home]}>
+    <Text style={styles.text}>This is my home screen :)</Text>
+    <Link href="/profile" style={styles.button}>
+      <Text style={styles.buttonText}>See profile</Text>
+    </Link>
+    <Link href="/overflowing" style={styles.button}>
+      <Text style={styles.buttonText}>See overflowing</Text>
+    </Link>
+    <Link href="/safeView" style={styles.button}>
+      <Text style={styles.buttonText}>See not overflowing</Text>
+    </Link>
+    <Pressable style={styles.button} onPress={() => alert('You pressed a button.')}>
+      <Text style={styles.buttonText}>Get an alert</Text>
+    </Pressable>
+  </View>
 );
