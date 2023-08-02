eb3da52d2134328883353592d89ff4161f1c53a1
Claire Squires <claires@theodo.co.uk>
2023-08-02 13:43:26 +0100

    feat: install expo router

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { Navigation } from './src/navigation/Navigation';
+import { Navigation } from '../src/navigation/Navigation';
 
 export default function App() {
   return <Navigation />;
--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -1,13 +1,8 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { MainStackNavigator } from './StackNavigator';
-
-const Tab = createBottomTabNavigator();
 
 export function Navigation() {
   return (
-    <NavigationContainer>
       <MainStackNavigator />
-    </NavigationContainer>
   );
 }
